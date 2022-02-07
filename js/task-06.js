59ddfcbbf581237f885a518d82e3be99bb307374
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-02-07 17:37:38 +0200

    исправлено 6-ое задание

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,16 @@ const inputLength = document.querySelector('input[data-length="6"]');
 input.addEventListener('blur', handleInputChange);
 
 function handleInputChange(event) {
+
+    if (input.classList.contains('valid') || input.classList.contains('invalid')) {
+        input.classList.remove('invalid');
+        input.classList.remove('valid');
+    }
     if (event.currentTarget.value.length === Number(inputLength.dataset.length)) {
+            
         input.classList.add('valid');
         
     }
     else 
         input.classList.add('invalid');
-       
-    
 }
